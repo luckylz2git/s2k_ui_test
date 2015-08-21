5b2f1f4a1c71e7af8600b090be40529cdb66526a
luckylz2git <8618993@qq.com>
2015-08-21 14:40:53 +0800

    testing products

--- bat/s2k_products_spec.js
+++ bat/s2k_products_spec.js
@@ -1016,7 +1016,7 @@ describe("s2k login page", function() {
     var topMenu = browser.params.menuindex.products.index;
     it("login to system", logins2k);
 
-    if (true) {
+    if (false) {
         describe('"Products" menu navigation', function () {
             var dropdown;
             beforeEach(function () {
@@ -1166,7 +1166,7 @@ describe("s2k login page", function() {
         });
     }
 
-    if (false) {
+    if (true) {
         describe('Products Unit Debug Test', function () {
             var dropdown;
             beforeEach(function () {
@@ -1179,8 +1179,8 @@ describe("s2k login page", function() {
             for (i = 0; i < testcount; i++) {
                 //闭包函数参考：http://stackoverflow.com/questions/21634558/looping-on-a-protractor-test-with-parameters
                 (function (testindex) {
-                    it('Tax Rate', function () {
-                        click_products_catalog(dropdown, browser.params.menuindex.products.idxTaxRates, testindex); //7=Tax Rate
+                    it('Department', function () {
+                        click_products_catalog(dropdown, browser.params.menuindex.products.idxDepartment, testindex); //2=Department
                     });
                 })(i);
             }
